f23c75016235900adecaeee81de6a25f6f758f24
Sizhe Zhao <prc.zhao@outlook.com>
2026-01-19 14:58:02 +0800

    modules/nixos: add roa checks

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,10 +1,18 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
   cfg = config.networking.dn42;
+
+  registry_wizard = lib.getExe pkgs.dn42_registry_wizard;
+  roa = pkgs.runCommand "roa" { } ''
+    mkdir -p $out
+    ${registry_wizard} ${cfg.roa.registry} roa v4 >$out/dn42_roa4.conf
+    ${registry_wizard} ${cfg.roa.registry} roa v6 >$out/dn42_roa6.conf
+  '';
 in
 {
   options.networking.dn42 = {
@@ -38,6 +46,13 @@ in
         type = lib.types.str;
         default = cfg.ipv4.address;
         description = "The router ID to use for BIRD.";
+      };
+    };
+    roa = {
+      enable = lib.mkEnableOption "ROA checks";
+      registry = lib.mkOption {
+        type = lib.types.path;
+        description = "Path to the DN42 registry.";
       };
     };
   };
@@ -84,6 +99,21 @@ in
           ipv6;
         }
 
+        ${lib.optionalString cfg.roa.enable ''
+          roa4 table dn42_roa4;
+          roa6 table dn42_roa6;
+
+          protocol static {
+            roa4 { table dn42_roa4; };
+            include "${roa}/dn42_roa4.conf";
+          }
+
+          protocol static {
+            roa6 { table dn42_roa6; };
+            include "${roa}/dn42_roa6.conf";
+          }
+        ''}
+
         template bgp dn42_peer {
           local as ${toString cfg.asn};
 
@@ -91,6 +121,9 @@ in
             extended next hop on;
 
             import filter {
+              ${lib.optionalString cfg.roa.enable ''
+                if roa_check(dn42_roa4) != ROA_VALID then reject;
+              ''}
               accept;
             };
 
@@ -101,6 +134,9 @@ in
 
           ipv6 {
             import filter {
+              ${lib.optionalString cfg.roa.enable ''
+                if roa_check(dn42_roa6) != ROA_VALID then reject;
+              ''}
               accept;
             };
 
