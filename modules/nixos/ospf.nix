6c72538e24fb7d614d0f44cbc4b504957c70c35d
Sizhe Zhao <prc.zhao@outlook.com>
2026-02-03 22:01:56 +0800

    modules/nixos/ospf: add ospfv2

--- modules/nixos/ospf.nix
+++ modules/nixos/ospf.nix
@@ -1,12 +1,51 @@
 { config, lib, ... }:
 let
   cfg = config.networking.dn42.ospf;
+  interfaces = lib.filterAttrs (_: { enable, ... }: enable) cfg.interfaces;
 in
 {
   options.networking.dn42.ospf = {
     enable = lib.mkEnableOption "OSPF";
+    interfaces = lib.mkOption {
+      description = "Interfaces to use for OSPF.";
+      type = lib.types.attrsOf (
+        lib.types.submodule (
+          { name, ... }:
+          {
+            options = {
+              enable = lib.mkEnableOption "enable the interface" // {
+                default = true;
+              };
+              name = lib.mkOption {
+                type = lib.types.str;
+                description = "Name of the interface.";
+                default = name;
+              };
+            };
+          }
+        )
+      );
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    services.bird.config = ''
+      protocol ospf v2 ospfv2 {
+        ipv4 {
+          export where source ~ [ RTS_DEVICE, RTS_OSPF, RTS_BGP ];
+        };
+        area 0 {
+          ${lib.concatMapAttrsStringSep "\n" (
+            _:
+            { name, ... }:
+            ''
+              interface "${name}" {
+                type ptp;
+              };
+            ''
+          ) interfaces}
+        };
+      }
+    '';
   };
 }
