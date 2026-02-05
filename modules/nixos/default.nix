3836ca77e1dc88615544848020812c7719563e22
Sizhe Zhao <prc.zhao@outlook.com>
2026-02-05 09:40:22 +0800

    modules/nixos/bgp: split

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,22 +1,11 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, ... }:
 let
   cfg = config.networking.dn42;
-
-  registry_wizard = lib.getExe pkgs.dn42_registry_wizard;
-  roa = pkgs.runCommand "roa" { } ''
-    mkdir -p $out
-    ${registry_wizard} ${cfg.roa.registry} roa v4 >$out/dn42_roa4.conf
-    ${registry_wizard} ${cfg.roa.registry} roa v6 >$out/dn42_roa6.conf
-  '';
 in
 {
   imports = [
     ./babel.nix
+    ./bgp.nix
     ./ospf.nix
   ];
 
@@ -51,13 +40,6 @@ in
       default = cfg.ipv4.address;
       description = "The router ID to use.";
     };
-    roa = {
-      enable = lib.mkEnableOption "ROA checks";
-      registry = lib.mkOption {
-        type = lib.types.path;
-        description = "Path to the DN42 registry.";
-      };
-    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -88,62 +70,6 @@ in
               krt_prefsrc = ${cfg.ipv6.address};
               accept;
             };
-          };
-        }
-
-        protocol static {
-          route ${cfg.ipv4.pool} unreachable;
-          ipv4;
-        }
-
-        protocol static {
-          route ${cfg.ipv6.pool} unreachable;
-          ipv6;
-        }
-
-        ${lib.optionalString cfg.roa.enable ''
-          roa4 table dn42_roa4;
-          roa6 table dn42_roa6;
-
-          protocol static {
-            roa4 { table dn42_roa4; };
-            include "${roa}/dn42_roa4.conf";
-          }
-
-          protocol static {
-            roa6 { table dn42_roa6; };
-            include "${roa}/dn42_roa6.conf";
-          }
-        ''}
-
-        template bgp dn42_peer {
-          local as ${toString cfg.asn};
-
-          ipv4 {
-            next hop self ebgp;
-            extended next hop on;
-
-            import filter {
-              ${lib.optionalString cfg.roa.enable ''
-                if roa_check(dn42_roa4) != ROA_VALID then reject;
-              ''}
-              accept;
-            };
-
-            export where (source = RTS_STATIC) || (source = RTS_BGP);
-          };
-
-          ipv6 {
-            next hop self ebgp;
-
-            import filter {
-              ${lib.optionalString cfg.roa.enable ''
-                if roa_check(dn42_roa6) != ROA_VALID then reject;
-              ''}
-              accept;
-            };
-
-            export where (source = RTS_STATIC) || (source = RTS_BGP);
           };
         }
       '';
